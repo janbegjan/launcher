9c11620177a1ce620baeffb014b4b989fcc7974b
janbegjan <janbeg2014@gmail.com>
2018-09-30 17:09:38 +0600

    sidebar added

--- footer.php
+++ footer.php
@@ -2,16 +2,18 @@
 		<div id="fh5co-footer">
 			<div class="row">
 				<div class="col-md-6">
-					<ul id="fh5co-social">
-						<li><a href="#"><i class="icon-facebook"></i></a></li>
-						<li><a href="#"><i class="icon-twitter"></i></a></li>
-						<li><a href="#"><i class="icon-instagram"></i></a></li>
-						<li><a href="#"><i class="icon-google-plus"></i></a></li>
-						<li><a href="#"><i class="icon-pinterest-square"></i></a></li>
-					</ul>
+          <?php
+            if(is_active_sidebar( 'sidebar_footer_left' )){
+              dynamic_sidebar('sidebar_footer_left');
+            }
+          ?>
 				</div>
 				<div class="col-md-6 fh5co-copyright">
-					<p>Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.com" target="_blank">Unsplash</a></p>
+          <?php
+            if(is_active_sidebar( 'sidebar_footer_right' )){
+              dynamic_sidebar('sidebar_footer_right');
+            }
+          ?>
 				</div>
 			</div>
 		</div>
